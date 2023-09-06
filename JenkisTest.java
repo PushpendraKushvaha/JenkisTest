6df08ffa7d8be83ba206ba3c3dc4b4dacbd92773
Pushpendra Kushvaha <pushpendra7096@gmail.com>
2023-09-06 16:57:49 +0530

    Added more

--- JenkisTest.java
+++ JenkisTest.java
@@ -2,5 +2,6 @@ public class JenkisTest {
     public static void main(String[] args) {
         System.out.println("Hello from java program");
         System.out.println("Added one more line");
+        System.out.println("Added to automatic");
     }
 }
